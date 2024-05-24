6ba3ff0dbd184ad0f9ec6844776e4295381433e9
lucas-albieri <lucas.albisantos55@gmail.com>
2024-05-24 18:10:52 -0300

    chore: Update dependencies and add Apollo client for GraphQL integration

---
+++ src/api/useFetchPokemons.ts
@@ -0,0 +1,10 @@
+// import { UseQueryResult, useQuery } from "@tanstack/react-query";
+// import { FetchPokemonsVariables, PokemonModel } from "../models/pokemon-model";
+// import { fetchPokemons } from "../queries/fetchPokemons";
+
+// export const useFetchPokemons = (variables: FetchPokemonsVariables): UseQueryResult<PokemonModel[], Error> => {
+//     return useQuery({
+//         queryKey: ['pokemons', variables],
+//         queryFn: fetchPokemons,
+//     });
+// };
